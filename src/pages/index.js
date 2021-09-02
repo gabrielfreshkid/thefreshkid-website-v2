a610ffec18ced7f92d65578f7ff889888aed14fc
gabriel-freshkid <gabrielfreshkid.com>
2021-09-02 13:55:43 +0100

    Removed old uses of updated components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import * as React from "react"
-import { Link, graphql } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/Layout"
-import Seo from "../components/seo"
+import { graphql } from "gatsby"
+import "./index.css"
 
 import Header from "../components/Header"
 import FeatureBar from "../components/FeatureBar"
 import Footer from "../components/Footer"
 import ValueProposition from "../components/ValueProposition"
-import index from "./index.css"
+
+
 import ContentFeature1 from "../components/ContentFeature1"
 import ContentFeature2 from "../components/ContentFeature2"
 import ContentFeature3 from "../components/ContentFeature3"
@@ -31,15 +29,7 @@ function IndexPage({ data }) {
               src={node.heroImage.file.url}
             />
           ))}
-
-          {/* <StaticImage
-            src="../images/gatsby-astronaut.png"
-            width={300}
-            quality={95}
-            formats={["AUTO", "WEBP", "AVIF"]}
-            alt="A Gatsby astronaut"
-            style={{ marginBottom: `1.45rem` }}
-            /> */}
+          
         </div>
         <ValueProposition></ValueProposition>
         <ContentFeature1></ContentFeature1>
